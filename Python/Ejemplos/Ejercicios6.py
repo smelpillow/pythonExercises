d61fd9bf3bf854528c0df8d669a1bab949c883f9
Manel <smelpillow@gmail.com>
2021-12-01 12:40:30 +0100

    Otros ejercicios

---
+++ Python/Ejemplos/Ejercicios6.py
@@ -0,0 +1,13 @@
+age = int(input("¿Cuál es tu edad? "))
+income = float(input("¿Cuales son tus ingresos mensuales?"))
+if age > 16 and income >= 1000:
+    print("Tienes que cotizar")
+else:
+    print("No tienes que cotizar")
+
+age = int(input("¿Cuál es tu edad? "))
+income = float(input("¿Cuales son tus ingresos mensuales?"))
+if age <= 16 or income < 1000:
+    print("No tienes que cotizar")
+else:
+    print("Tienes que cotizar")
