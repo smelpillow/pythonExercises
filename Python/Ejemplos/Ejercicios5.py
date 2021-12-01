a7f373a7b15c54d45b569298342b466a76fdc946
Manel <smelpillow@gmail.com>
2021-12-01 12:43:17 +0100

    Ok

---
+++ Python/Ejemplos/Ejercicios5.py
@@ -0,0 +1,25 @@
+age = int(input("¿Cuál es tu edad? "))
+if age < 18: 
+    print ("Eres menor de edad.")
+else:
+    print("Eres mayor de edad.")
+
+key = "contraseña"
+password = input("Introduce la contraseña: ")
+if key == password.lower():
+    print("La contaseña coincide")
+else:
+    print("La contraseña no coincide")
+
+n = float(input("Introduce el dividendo: "))
+m = float(input("Introduce el divisior: "))
+if m == 0:
+    print("¡Error! No se puede dividir por 0.")
+else:
+    print(n/m)
+
+n = int(input("Introduce un número entero: "))
+if n % 2 == 0:
+    print("El número " + str(n) + " es par")
+else:
+    print("El número " + str(n) + " es impar")
