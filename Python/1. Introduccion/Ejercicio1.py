84058f6624e95b1f34c1ec7eb05c568fa9e85a28
Manel <smelpillow@gmail.com>
2021-12-06 20:31:34 +0100

    Mas ejercicios

---
+++ Python/1. Introduccion/Ejercicio1.py
@@ -0,0 +1,11 @@
+num2 = 2
+num3 = 3
+num5 = 5
+
+print ( ( ( num3 + num2 ) / ( num2 * num5 ) ) ** num2 )
+
+print ((num3 + num2) / (num2 * num5) / num2)
+
+calculo = pow(((3+2)/(2*5)),2) # Este pow hace lo mismo que lo anterior pero de otra manera
+
+print (calculo)
