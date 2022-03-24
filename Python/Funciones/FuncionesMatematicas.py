fc3b106a03c912dce6f508b8e625bee96812cda2
Manel <smelpillow@gmail.com>
2022-03-24 15:37:44 +0100

    Ok

---
+++ Python/Funciones/FuncionesMatematicas.py
@@ -0,0 +1,13 @@
+import math
+import random
+
+print(math.pow(10, 2)) # Multiplicamos el primer valor X veces segun el segundo valor
+print(pow(10, 2))
+print(math.sqrt(64)) # Sacamos la raiz cuadrada del valor indicado
+print(math.isqrt(64)) # Raiz cuadrada resultado valor entero
+print(math.sin(90)) # El seno
+print(math.cos(90)) # El coseno
+print(math.tan(64)) # la tangente
+print(math.factorial(5)) # 1 * 2 * 3 * 4 * 5
+
+print(random.randint(1, 100))
---
+++ Python/Funciones/Retorno.py
@@ -0,0 +1,23 @@
+def entero():
+    print("Mensaje:")
+    return 10
+
+def decimal():
+    print("Mensaje 2:")
+    return 90.99
+
+print(entero())
+decimal()
+
+def frase():
+    return 'Bro que pacha'
+
+frase()
+
+def asignacion():
+    return 1, 2, 3, 4, 5
+
+a, b, c, d, e = asignacion()
+
+print(a)
+print(c)
