6ecce55794cd324be837f4514a0e7bbf9a57fcc4
Manel <smelpillow@gmail.com>
2022-04-08 01:24:36 +0200

    Too much

---
+++ Python/EJercicios POO/Ejercicio2.py
@@ -0,0 +1,31 @@
+# Realizar un programa en el cual se declaren dos valores enteros por teclado utilizando el método __init__. 
+# Calcular después la suma, resta, multiplicación y división. Utilizar un método para cada una e imprimir 
+# los resultados obtenidos. Llamar a la clase Calculadora.
+
+class Calculadora():
+    def __init__(self):
+        self.num1 = int(input("Ingrese el primer valor: "))
+        self.num2 = int(input("Ingrese el segundo valor: "))
+
+    def suma(self):
+        self.suma = self.num1 + self.num2
+        print("La suma da como resultado: ", self.suma)
+
+    def resta(self):
+        self.resta = self.num1 - self.num2
+        print("La resta da como resultado: ", self.resta)
+    
+    def multi(self):
+        self.multi = self.num1 * self.num2
+        return "La multiplicación da como resultado: ", self.multi
+    
+    def division(self):
+        self.division = self.num1 / self.num2
+        return "La división da como resultado: ", self.division
+
+
+calcular = Calculadora()
+calcular.suma()
+calcular.resta()
+print(calcular.multi())
+print(calcular.division())
